addd2526921acf7232821c84f329e94cd446022c
Eli Collins <eli@cloudera.com>
2011-05-25 23:08:42 -0700

    Export HADOOP_OPTS

--- bin/hadoop-home.sh
+++ bin/hadoop-home.sh
@@ -3,3 +3,4 @@ HADOOP_SRC_ROOT=/home/eli/src
 export HADOOP_COMMON_HOME=$HADOOP_SRC_ROOT/common$1
 export HADOOP_HDFS_HOME=$HADOOP_SRC_ROOT/hdfs$1
 export HADOOP_MAPRED_HOME=$HADOOP_SRC_ROOT/mapreduce$1
+export HADOOP_OPTS="$HADOOP_OPTS -Djava.security.egd=file:///dev/urandom"
