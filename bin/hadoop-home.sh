96806c8a8865c66851850dc8150d45bdd480ae7d
Eli Collins <eli@cloudera.com>
2011-06-21 15:17:31 -0700

    Update home setting based on new unsplit dir structure.

--- bin/hadoop-home.sh
+++ bin/hadoop-home.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-HADOOP_SRC_ROOT=/home/eli/src
-export HADOOP_COMMON_HOME=$HADOOP_SRC_ROOT/common$1
-export HADOOP_HDFS_HOME=$HADOOP_SRC_ROOT/hdfs$1
-export HADOOP_MAPRED_HOME=$HADOOP_SRC_ROOT/mapreduce$1
+HADOOP_SRC_ROOT=/home/eli/src/hadoop$1
+export HADOOP_COMMON_HOME=$HADOOP_SRC_ROOT/common
+export HADOOP_HDFS_HOME=$HADOOP_SRC_ROOT/hdfs
+export HADOOP_MAPRED_HOME=$HADOOP_SRC_ROOT/mapreduce
 export HADOOP_OPTS="$HADOOP_OPTS -Djava.security.egd=file:///dev/urandom"
